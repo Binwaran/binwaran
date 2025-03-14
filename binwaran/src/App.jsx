743747048f6b04dfc6692aa912f6f28fe3fc6310
Binwaran <binwaran.m@gmail.com>
2025-03-14 22:57:33 +0800

    feat: home page

--- binwaran/src/App.jsx
+++ binwaran/src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,10 +7,15 @@ import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import TechStack from "./pages/TechStack";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import AdminPanel from "./pages/AdminPanel";
+import Article from "./pages/Article";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Notifications from "./pages/Notifications";
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router> {/* Router ต้องเป็นตัวครอบหลัก */}
       <Navbar />
@@ -24,32 +26,15 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/techstack" element={<TechStack />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/notifications" element={<Notifications/>} />
       </Routes>
-
-      {/* ส่วนอื่นๆ ที่ไม่เกี่ยวกับ Router ให้อยู่นอก <Routes> */}
-      <div className="text-amber-500 text-8xl bg-white">
-        Welcome to my House!!!
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Home />
+     
     </Router>
   );
 }
